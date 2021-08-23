1b75f112d7fd1779306c570585ceb8150ed436d0
Cristhian Carpio <Cristhian.carpio18@gmail.com>
2021-08-23 00:07:59 -0500

    Se crea app shippingmanagement: Este módulo permite la administración de envíos

---
+++ shippingmanagement/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from shippingmanagement.models import StateShipment
+from ordermanagement.models import Order
+# Create your models here.
+class Shipment(models.Model):
+    id = models.AutoField(primary_key = True)
+    departure_datetime = models.DateTimeField(max_length=50)
+    state = models.ForeignKey(StateShipment, blank=False, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, blank=False, on_delete=models.CASCADE)
+    def __str__(self):
+        return (self.name)

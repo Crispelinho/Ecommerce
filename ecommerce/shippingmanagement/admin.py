63d2ea9e0288535b14874a0e91e3795f3ad9ff12
Cristhian Carpio <Cristhian.carpio18@gmail.com>
2021-08-23 01:04:32 -0500

    Se añade admin de shipment

--- ecommerce/shippingmanagement/admin.py
+++ ecommerce/shippingmanagement/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from shippingmanagement.models import *
 # Register your models here.
+class ShipmentAdmin(admin.ModelAdmin):
+    list_display=("id","departure_datetime","state","order")
+    search_fields=("departure_datetime","state")
+    list_filter=("state",)
+    icon_name = 'person'
+    list_per_page=10
+
+admin.site.register(Shipment,ShipmentAdmin)

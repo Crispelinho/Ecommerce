0f59f06a90f382185fbcad5bf7b285c3037a1260
Cristhian Carpio <Cristhian.carpio18@gmail.com>
2021-08-23 23:33:39 -0500

    Se suben cambios para configuración de ambientes de DEV y PROD.

--- ecommerce/ecommerce/settings/development.py
+++ ecommerce/ecommerce/settings/development.py
@@ -9,15 +9,15 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-      # 'ENGINE': 'django.db.backends.sqlite3',
-      # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
 
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'qualit_qadmission',       
-        'USER': 'root',  
-        'PASSWORD': '',  
-        'HOST': 'localhost', 
-        'PORT': '3306', 
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'qualit_ecommerce',       
+        # 'USER': 'root',  
+        # 'PASSWORD': '',  
+        # 'HOST': 'localhost', 
+        # 'PORT': '3306', 
 
     }
 }
--- ecommerce/manage.py
+++ ecommerce/manage.py
@@ -11,7 +11,7 @@ def main(arg2):
         environment='production'
         sys.argv.pop(arg2)
 
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings.'+environment)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
